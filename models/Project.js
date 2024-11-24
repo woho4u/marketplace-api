2bac95d0c950ce3b6ebc254a8d4753dbdef761b2
Oliver Eriksen <okebedrift@gmail.com>
2024-11-24 14:43:22 +0100

    create Project model

---
+++ models/Project.js
@@ -0,0 +1,31 @@
+const { Schema, model, models } = require("mongoose");
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema(
+   {
+      title: { type: String, required: true },
+      description: { type: String, required: true },
+      images: { type: [String], required: false },
+      price: { type: Number, required: true },
+      category: { type: String, required: true },
+      creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+      location: { type: String },
+      materials: { type: [String] },
+      dimensions: {
+         height: { type: Number },
+         width: { type: Number },
+         depth: { type: Number },
+      },
+      available: { type: Boolean, default: true },
+      tags: { type: [String] },
+      dateCreated: { type: Date, default: Date.now },
+      ratings: {
+         average: { type: Number },
+         reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+      },
+   },
+   { collection: "projects" }
+);
+
+// Avoid overwriting models when hot-reloading in development
+module.exports = mongoose.models.Project || model("Project", ProjectSchema);
